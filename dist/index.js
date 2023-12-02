f82a744f847f691d268d10e0ee6f230ae0814681
jerry5379 <kadampritesh46@gmail.com>
2023-12-02 20:50:05 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Search", {
+  enumerable: true,
+  get: function get() {
+    return _icons["default"];
+  }
+});
+var _icons = _interopRequireDefault(require("./icons"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
